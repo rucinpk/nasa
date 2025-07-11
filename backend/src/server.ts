13dbf90f781863504d01e7b18c605f981515a5fa
Patryk Ruciński <rucinpk@gmail.com>
2025-07-11 20:54:39 +0200

    updated port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -9,7 +9,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 80;
 const NASA_API_KEY = process.env.NASA_API_KEY || 'DEMO_KEY';
 
 // Configure CORS allowed domains
